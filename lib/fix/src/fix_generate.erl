184cf79cb12c15ea649fbe77b22f6f2836034b47
Daniel Luna <daniel@lunas.se>
2011-07-09 01:43:51 +0000

    Generating files that compile.

--- lib/fix/src/fix_generate.erl
+++ lib/fix/src/fix_generate.erl
@@ -6,6 +6,34 @@
 -author('Daniel Luna <daniel@lunas.se>').
 -export([generate/1]).
 
+-include_lib("xmerl/include/xmerl.hrl").
+
 generate(XmlFile) ->
   {Xml, ""} = xmerl_scan:file(XmlFile),
-  io:format("~p", [Xml]).
+  Version = version(Xml),
+  Header =
+    ["%% -*- erlang-indent-level: 2 -*-\n"
+     "%% @author Daniel Luna <daniel@lunas.se>\n"
+     "%% @copyright 2011 Daniel Luna\n\n"
+     "-module(", string:to_lower(Version),
+     ").\s"
+     "-author('Daniel Luna <daniel@lunas.se>').\n"
+     "-export([decode/1]).\n",
+
+     "decode(_) -> ok."],
+
+  io:format("~s", [Header]).
+
+version(#xmlElement{attributes = Attrs}) ->
+  lists:flatten(
+    [attr(type, Attrs),
+     attr(major, Attrs),
+     attr(minor, Attrs),
+     case attr(servicepack, Attrs) of
+       "0" -> [];
+       SP -> ["SP", SP]
+     end]).
+
+attr(Key, L) ->
+  {value, Result} = lists:keysearch(Key, 2, L),
+  Result#xmlAttribute.value.
