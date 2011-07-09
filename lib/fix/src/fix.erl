3daac46724b5823c7cedd4b2ee755019c2defba8
Daniel Luna <daniel@lunas.se>
2011-07-09 03:07:35 +0000

    Adding a selector for FIX version.

--- lib/fix/src/fix.erl
+++ lib/fix/src/fix.erl
@@ -7,7 +7,16 @@
 -export([decode/1, start/0]).
 
 decode(String) ->
-  throw(not_yet_implemented).
+  case String of
+    "8=FIX.4.0" ++ [1] ++ _ -> fix40:decode(String);
+    "8=FIX.4.1" ++ [1] ++ _ -> fix41:decode(String);
+    "8=FIX.4.2" ++ [1] ++ _ -> fix42:decode(String);
+    "8=FIX.4.3" ++ [1] ++ _ -> fix43:decode(String);
+    "8=FIX.4.4" ++ [1] ++ _ -> fix44:decode(String);
+    "8=FIX.5.0" ++ [1] ++ _ -> fix50:decode(String);
+    "8=FIX.5.0SP1" ++ [1] ++ _ -> fix50sp1:decode(String);
+    "8=FIX.5.0SP2" ++ [1] ++ _ -> fix50sp2:decode(String)
+  end.
 
 start() ->
   ok.
