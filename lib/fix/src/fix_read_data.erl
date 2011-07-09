ef23f37e5e4d5ea69a381f3ebbc02bfba75b5a26
Daniel Luna <daniel@lunas.se>
2011-07-09 22:48:45 +0000

    Alphabetic sorting of types.

--- lib/fix/src/fix_read_data.erl
+++ lib/fix/src/fix_read_data.erl
@@ -16,49 +16,49 @@
 
 %% Char type in fix 4.0 looks a lot like a string in 4.2 and later
 fix40(char, Data) -> string(Data);
-fix40(int, Data) -> int(Data);
 fix40(float, Data) -> float(Data);
+fix40(int, Data) -> int(Data);
 fix40(time, Data) -> time(Data);
 fix40(Type, Data) -> throw({not_yet_implemented_type, fix40, Type, Data}).
 
 fix41(char, Data) -> string(Data);
-fix41(int, Data) -> int(Data);
 fix41(float, Data) -> float(Data);
+fix41(int, Data) -> int(Data);
 fix41(time, Data) -> time(Data);
 fix41(Type, Data) -> throw({not_yet_implemented_type, fix41, Type, Data}).
 
-fix42(char, Data) -> char(Data);
-fix42(string, Data) -> string(Data);
-fix42(int, Data) -> int(Data);
 fix42(boolean, Data) -> boolean(Data);
+fix42(char, Data) -> char(Data);
+fix42(int, Data) -> int(Data);
+fix42(string, Data) -> string(Data);
 fix42(utctimestamp, Data) -> time(Data);
 fix42(Type, Data) -> throw({not_yet_implemented_type, fix42, Type, Data}).
 
 fix43(boolean, Data) -> boolean(Data);
 fix43(char, Data) -> char(Data);
-fix43(string, Data) -> string(Data);
-fix43(length, Data) -> length(Data);
 fix43(int, Data) -> int(Data);
+fix43(length, Data) -> length(Data);
 fix43(seqnum, Data) -> seqnum(Data);
+fix43(string, Data) -> string(Data);
 fix43(utctimestamp, Data) -> time(Data);
 fix43(Type, Data) -> throw({not_yet_implemented_type, fix43, Type, Data}).
 
 fix44(boolean, Data) -> boolean(Data);
 fix44(char, Data) -> char(Data);
 fix44(int, Data) -> int(Data);
-fix44(string, Data) -> string(Data);
 fix44(length, Data) -> length(Data);
 fix44(seqnum, Data) -> seqnum(Data);
+fix44(string, Data) -> string(Data);
 fix44(utctimestamp, Data) -> time(Data);
 fix44(Type, Data) -> throw({not_yet_implemented_type, fix44, Type, Data}).
 
+fixt11(boolean, Data) -> boolean(Data);
 fixt11(int, Data) -> int(Data);
 fixt11(length, Data) -> int(Data); %% length_(Data);
-fixt11(tagnum, Data) -> int(Data); %% tagnum(Data);
-fixt11(seqnum, Data) -> int(Data); %% seqnum(Data);
 fixt11(numingroup, Data) -> int(Data); %% numingroup(Data);
+fixt11(seqnum, Data) -> int(Data); %% seqnum(Data);
 fixt11(string, Data) -> string(Data);
-fixt11(boolean, Data) -> boolean(Data);
+fixt11(tagnum, Data) -> int(Data); %% tagnum(Data);
 fixt11(utctimestamp, Data) -> time(Data);
 fixt11(Type, Data) -> throw({not_yet_implemented_type, fixt11, Type, Data}).
 
@@ -66,6 +66,14 @@ boolean([$Y, ?SOH | Rest]) -> {true, Rest};
 boolean([$N, ?SOH | Rest]) -> {false, Rest}.
 
 char([C, ?SOH | Rest]) -> {C, Rest}.
+
+float(Data) ->
+  {String, Rest} = string(Data),
+  {list_to_float(String), Rest}.
+
+int(Data) ->
+  {String, Rest} = string(Data),
+  {list_to_integer(String), Rest}.
 
 length(Data) -> posint(Data).
 
@@ -83,15 +91,6 @@ string(Data) -> string(Data, []).
 
 string([?SOH | Rest], Acc) -> {lists:reverse(Acc), Rest};
 string([C | Rest], Acc) -> string(Rest, [C | Acc]).
-
-int(Data) ->
-  {String, Rest} = string(Data),
-  {list_to_integer(String), Rest}.
-
-float(Data) ->
-  {String, Rest} = string(Data),
-  {list_to_float(String), Rest}.
-  
 
 time(Data) ->
   {String, Rest} = string(Data),
