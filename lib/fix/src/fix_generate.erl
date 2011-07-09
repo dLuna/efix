424ca63752775a563ceb18623b56238ded913981
Daniel Luna <daniel@lunas.se>
2011-07-09 18:52:17 +0000

    Refactoring.

--- lib/fix/src/fix_generate.erl
+++ lib/fix/src/fix_generate.erl
@@ -17,48 +17,112 @@ generate(XmlFile) ->
   {value, {source, Source}} = lists:keysearch(source, 1, Compile),
   {ok, Bin} = file:read_file(filename:dirname(Source) ++ "/fix_generate.aux"),
   Parser = binary_to_list(Bin),
+
   Result = set_values(Parser, Xml),
   io:format("~s", [Result]).
 
-set_values([], _) -> [];
 set_values("%%VERSIONATOM%%" ++ Rest, Xml) ->
-  [version_atom(Xml), set_values(Rest, Xml)];
+  [generate_fix_version(Xml), set_values(Rest, Xml)];
 set_values("%%HEADERPARSER%%" ++ Rest, Xml) ->
-  [header_parser(Xml), set_values(Rest, Xml)];
+  [generate_header_parser(Xml), set_values(Rest, Xml)];
 set_values("%%MESSAGEPARSER%%" ++ Rest, Xml) ->
-  [message_parser(Xml), set_values(Rest, Xml)];
+  [generate_message_parser(Xml), set_values(Rest, Xml)];
 set_values("%%FOOTERPARSER%%" ++ Rest, Xml) ->
-  [footer_parser(Xml), set_values(Rest, Xml)];
+  [generate_footer_parser(Xml), set_values(Rest, Xml)];
 set_values("%%ENUM_TO_VALUE%%" ++ Rest, Xml) ->
-  [enum_to_value(Xml), set_values(Rest, Xml)];
+  [generate_enum_to_value(Xml), set_values(Rest, Xml)];
 set_values([C | Rest], Xml) ->
-  [C | set_values(Rest, Xml)].
+  [C | set_values(Rest, Xml)];
+set_values([], _) -> [].
 
-version_atom(Xml) ->
-  lists:flatten(
-    [to_lower(attr(type, Xml)),
-     attr(major, Xml),
-     attr(minor, Xml),
-     case attr(servicepack, Xml) of
-       "0" -> [];
-       SP -> ["sp", SP]
-     end]).
+generate_fix_version(Xml) ->
+  [string:to_lower(attr(type, Xml)),
+   attr(major, Xml),
+   attr(minor, Xml),
+   case attr(servicepack, Xml) of
+     "0" -> [];
+     SP -> ["sp", SP]
+   end].
 
-attr(Key, #xmlElement{attributes = Attrs}) -> attr(Key, Attrs);
-attr(Key, L) ->
-  {value, Result} = lists:keysearch(Key, #xmlAttribute.name, L),
+generate_header_parser(Xml) ->
+  [[single_field_parser("header", Element, Xml) ||
+     #xmlElement{name = field} = Element <- children_of_child(header, Xml)],
+   "header(Rest, Acc) ->\n"
+   "  {Rest, Acc}.\n"].
+
+generate_message_parser(Xml) ->
+  [[generate_message_typed_dispatcher(attr(name, Element), Xml) ||
+     #xmlElement{name = message} = Element <- children_of_child(messages, Xml)],
+   "message(MsgType, _Data) ->\n"
+   "  throw({unknown_message_type, MsgType}).\n\n",
+   [generate_message_type_handler(Element, Xml) ||
+     #xmlElement{name = message} = Element <- children_of_child(messages, Xml)]].
+
+generate_message_typed_dispatcher(Name, _Xml) ->
+  MsgType = camel_case_to_underscore(Name),
+  ["message(", MsgType, ", Data) ->\n"
+   "  ", MsgType, "(Data, []);\n"].
+
+generate_message_type_handler(Msg, Xml) ->
+  MsgName = camel_case_to_underscore(attr(name, Msg)),
+  [[single_field_parser(MsgName, Field, Xml) 
+    || #xmlElement{name = field} = Field <- Msg#xmlElement.content],
+   MsgName, "(Rest, Acc) ->\n"
+   "  {Rest, Acc}.\n"].
+
+generate_footer_parser(_) ->
+  %% FIXME
+  [].
+
+single_field_parser(MsgName, Field, Xml) ->
+  FieldName = attr(name, Field),
+  FieldTypeData = field_type_data(FieldName, Xml),
+  N = attr(number, FieldTypeData),
+  Type = string:to_lower(attr(type, FieldTypeData)),
+  FieldKey = camel_case_to_underscore(FieldName),
+  FixVersion = generate_fix_version(Xml),
+  MaybeEnumToValue =
+    case [Enum || #xmlElement{name = value} = Enum <-
+                    FieldTypeData#xmlElement.content] of
+      [] -> "  FieldData = FieldData0,\n";
+      _ -> ["  FieldData = enum_to_value(\"", N, "\", FieldData0),\n"]
+    end,
+  [MsgName, "(\"", N, "=\" ++ String, Acc) ->\n"
+   "  {FieldData0, Rest} = fix_read_data:", FixVersion, "(",
+   Type, ", String),\n",
+   MaybeEnumToValue,
+   "  ", MsgName, "(Rest, [{", FieldKey, ", FieldData} | Acc]);\n"].
+
+generate_enum_to_value(Xml) ->
+  [[generate_enum_to_value_field(Field) ||
+     #xmlElement{name = field} = Field <- children_of_child(fields, Xml)],
+   "enum_to_value(Type, Value) ->\n"
+   "  throw({unknown_enum_value, Type, Value}).\n"].
+
+generate_enum_to_value_field(Field) ->
+  N = attr(number, Field),
+  [["enum_to_value(\"", N, "\", \"", attr(enum, Enum), "\") ->\n"
+    "  ", quote_atom(string:to_lower(attr(description, Enum))), ";\n"] ||
+    #xmlElement{name = value} = Enum <- Field#xmlElement.content].
+
+%% XML helper functions:
+
+attr(Key, #xmlElement{attributes = Attrs}) ->
+  {value, Result} = lists:keysearch(Key, #xmlAttribute.name, Attrs),
   Result#xmlAttribute.value.
 
-name(Key, #xmlElement{content = Content}) -> name(Key, Content);
-name(Key, L) ->
-  {value, Result} = lists:keysearch(Key, #xmlElement.name, L),
+children_of_child(Key, #xmlElement{content = Content}) ->
+  {value, Result} = lists:keysearch(Key, #xmlElement.name, Content),
   Result#xmlElement.content.
 
-header_parser(Xml = #xmlElement{}) ->
-  [single_field_parser("header", Element, Xml) ||
-    #xmlElement{name = field} = Element <- name(header, Xml)].
-   
-field_name_to_atom([C | Rest]) ->
+field_type_data(Name, Xml) ->  
+  [V] = [Element || #xmlElement{} = Element <- children_of_child(fields, Xml),
+                    attr(name, Element) =:= Name],
+  V.
+      
+%% String functions
+
+camel_case_to_underscore([C | Rest]) ->
   [C + 32 | remove_camel_case(Rest)].
 
 remove_camel_case([C | Rest]) when C >= $A, C =< $Z ->
@@ -66,79 +130,6 @@ remove_camel_case([C | Rest]) when C >= $A, C =< $Z ->
 remove_camel_case([C | Rest]) -> 
   [C | remove_camel_case(Rest)];
 remove_camel_case([]) -> [].
-
-to_lower([C | Rest]) when C >= $A, C =< $Z ->
-  [C + 32 | to_lower(Rest)];
-to_lower([C | Rest]) ->
-  [C | to_lower(Rest)];
-to_lower([]) -> [].
-
-field_type_data(Name, Xml) ->  
-  with_attr_value(name, Name, name(fields, Xml)).
-
-with_attr_value(Attr, Value, Content) ->
-  case [Element || #xmlElement{attributes = Attributes} = Element <- Content,
-                   attr(Attr, Attributes) =:= Value] of
-    [] -> throw({Attr, Value, Content});
-    [V] -> V
-  end.
-      
-message_parser(Xml) ->
-  [[message_dispatch(attr(name, Element), Xml) ||
-     #xmlElement{name = message} = Element <- name(messages, Xml)],
-   "message(MsgType, _Data) ->\n"
-   "  throw({unknown_message_type, MsgType}).\n\n",
-   [message_handler(Element, Xml) ||
-     #xmlElement{name = message} = Element <- name(messages, Xml)]].
-
-message_dispatch(Name, _Xml) ->
-  Atom = field_name_to_atom(Name),
-  ["message(", Atom, ", Data) ->\n"
-   "  ", Atom, "(Data, []);\n"].
-
-message_handler(Msg, Xml) ->
-  MsgName = field_name_to_atom(attr(name, Msg)),
-  [[single_field_parser(MsgName, Field, Xml) 
-    || #xmlElement{name = field} = Field <- Msg#xmlElement.content],
-   MsgName, "(Rest, Acc) ->\n"
-   "  {Rest, Acc}.\n"].
-
-single_field_parser(MsgName, Field, Xml) ->
-  FieldName = attr(name, Field),
-  FieldTypeData = field_type_data(FieldName, Xml),
-  N = attr(number, FieldTypeData),
-  Type = attr(type, FieldTypeData),
-  AtomName = field_name_to_atom(FieldName),
-  Enum = [Enu || #xmlElement{name = value} = Enu <-
-                   FieldTypeData#xmlElement.content],
-  [MsgName, "(\"", N, "=\" ++ String, Acc) ->\n"
-   "  {FieldData0, Rest} = fix_read_data:", version_atom(Xml), "(",
-   to_lower(Type), ", String),\n",
-   case Enum of
-     [] -> "  FieldData = FieldData0,\n";
-     _ ->
-       ["  FieldData = enum_to_value(\"", N, "\", FieldData0),\n"]
-   end,
-   "  ", MsgName, "(Rest, [{", AtomName, ", FieldData} | Acc]);\n"].
-
-
-footer_parser(_) ->
-  [].
-
-enum_to_value(Xml) ->
-  [[enum(attr(number, Field), Field#xmlElement.content) ||
-     #xmlElement{name = field} = Field <- name(fields, Xml)],
-   "enum_to_value(Type, Value) ->\n"
-   "  throw({unknown_enum_value, Type, Value}).\n"].
-
-enum(N, L) ->
-  [enum_str(N, attr(enum, Enum),
-            quote_atom(to_lower(attr(description, Enum)))) ||
-    #xmlElement{name = value} = Enum <- L].
-
-enum_str(N, Enum, Desc) ->
-  ["enum_to_value(\"", N, "\", \"", Enum, "\") ->\n"
-   "  ", Desc, ";\n"].
 
 quote_atom("and") -> "'and'";
 quote_atom("or") -> "'or'";
