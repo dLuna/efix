f0da6812f5bc8d92a402fd5144bf0f46dca16317
Daniel Luna <daniel@lunas.se>
2011-07-09 17:23:59 +0000

    Yes, things don't really work at the moment, but I've been going too long without a commit.

--- lib/fix/src/fix_generate.erl
+++ lib/fix/src/fix_generate.erl
@@ -25,6 +25,12 @@ set_values("%%VERSIONATOM%%" ++ Rest, Xml) ->
   [version_atom(Xml), set_values(Rest, Xml)];
 set_values("%%HEADERPARSER%%" ++ Rest, Xml) ->
   [header_parser(Xml), set_values(Rest, Xml)];
+set_values("%%MESSAGEPARSER%%" ++ Rest, Xml) ->
+  [message_parser(Xml), set_values(Rest, Xml)];
+set_values("%%FOOTERPARSER%%" ++ Rest, Xml) ->
+  [footer_parser(Xml), set_values(Rest, Xml)];
+set_values("%%ENUM_TO_VALUE%%" ++ Rest, Xml) ->
+  [enum_to_value(Xml), set_values(Rest, Xml)];
 set_values([C | Rest], Xml) ->
   [C | set_values(Rest, Xml)].
 
@@ -54,13 +60,21 @@ header_parser(Xml = #xmlElement{}) ->
 
 header(E = #xmlElement{}, Xml) ->
   Name = attr(name, E),
-  Field = with_attr_value(name, Name, name(fields, Xml)),
-  N = attr(number, Field),
-  Type = attr(type, Field),
+  FieldTypeData = field_type_data(Name, Xml),
+  N = attr(number, FieldTypeData),
+  Type = attr(type, FieldTypeData),
+  AtomName = field_name_to_atom(Name),
+  Enum = [Enu || #xmlElement{name = value} = Enu <-
+                   FieldTypeData#xmlElement.content],
   ["header(\"", N, "=\" ++ String, Acc) ->\n"
-   "  {FieldData, Rest} = fix_read_data:", version_atom(Xml), "(",
-   to_lower(Type), ", String),\n"
-   "  header(Rest, [{", field_name_to_atom(Name), ", FieldData} | Acc]);\n"].
+   "  {FieldData0, Rest} = fix_read_data:", version_atom(Xml), "(",
+   to_lower(Type), ", String),\n",
+   case Enum of
+     [] -> "  FieldData = FieldData0,\n";
+     _ ->
+       ["  FieldData = enum_to_value(", N, ", FieldData0),\n"]
+   end,
+   "  header(Rest, [{", AtomName, ", FieldData} | Acc]);\n"].
    
 field_name_to_atom([C | Rest]) ->
   [C + 32 | remove_camel_case(Rest)].
@@ -77,6 +91,9 @@ to_lower([C | Rest]) ->
   [C | to_lower(Rest)];
 to_lower([]) -> [].
 
+field_type_data(Name, Xml) ->  
+  with_attr_value(name, Name, name(fields, Xml)).
+
 with_attr_value(Attr, Value, Content) ->
   case [Element || #xmlElement{attributes = Attributes} = Element <- Content,
                    attr(Attr, Attributes) =:= Value] of
@@ -84,3 +101,66 @@ with_attr_value(Attr, Value, Content) ->
     [V] -> V
   end.
       
+message_parser(Xml) ->
+  [[message_dispatch(attr(name, Element), Xml) ||
+     #xmlElement{name = message} = Element <- name(messages, Xml)],
+   "message(MsgType, _Data) ->\n"
+   "  throw({unknown_message_type, MsgType}).\n\n",
+   [message_handler(Element, Xml) ||
+     #xmlElement{name = message} = Element <- name(messages, Xml)]].
+
+message_dispatch(Name, _Xml) ->
+  Atom = field_name_to_atom(Name),
+  ["message(", Atom, ", Data) ->\n"
+   "  ", Atom, "(Data, []);\n"].
+
+message_handler(Msg, Xml) ->
+  MsgName = field_name_to_atom(attr(name, Msg)),
+  [[single_message(MsgName, Field, Xml) 
+    || #xmlElement{name = field} = Field <- Msg#xmlElement.content],
+   MsgName, "(Rest, Acc) ->\n"
+   "  {Rest, Acc}.\n"].
+
+
+single_message(MsgName, Field, Xml) ->
+  FieldName = attr(name, Field),
+  FieldTypeData = field_type_data(FieldName, Xml),
+  N = attr(number, FieldTypeData),
+  Type = attr(type, FieldTypeData),
+  AtomName = field_name_to_atom(FieldName),
+  Enum = [Enu || #xmlElement{name = value} = Enu <-
+                   FieldTypeData#xmlElement.content],
+  [MsgName, "(\"", N, "=\" ++ String, Acc) ->\n"
+   "  {FieldData0, Rest} = fix_read_data:", version_atom(Xml), "(",
+   to_lower(Type), ", String),\n",
+   case Enum of
+     [] -> "  FieldData = FieldData0,\n";
+     _ ->
+       ["  FieldData = enum_to_value(\"", N, "\", FieldData0),\n"]
+   end,
+   "  ", MsgName, "(Rest, [{", AtomName, ", FieldData} | Acc]);\n"].
+
+
+footer_parser(_) ->
+  [].
+
+enum_to_value(Xml) ->
+  [[enum(attr(number, Field), Field#xmlElement.content) ||
+     #xmlElement{name = field} = Field <- name(fields, Xml)],
+   "enum_to_value(Type, Value) ->\n"
+   "  throw({unknown_enum_value, Type, Value}).\n"].
+
+enum(N, L) ->
+  [enum_str(N, attr(enum, Enum),
+            quote_atom(to_lower(attr(description, Enum)))) ||
+    #xmlElement{name = value} = Enum <- L].
+
+enum_str(N, Enum, Desc) ->
+  ["enum_to_value(\"", N, "\", \"", Enum, "\") ->\n"
+   "  ", Desc, ";\n"].
+
+quote_atom("receive") -> "'receive'";
+quote_atom([C | Rest]) when C < $a orelse C > $z ->
+  [$', C, Rest, $'];
+quote_atom(String) ->
+  String.
