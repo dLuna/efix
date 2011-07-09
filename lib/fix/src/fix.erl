59ffed9e7cd21353b6a29d3903eb020f0c5f1a28
Daniel Luna <daniel@lunas.se>
2011-07-09 00:01:44 +0000

    Adding directory structure.

---
+++ lib/fix/src/fix.erl
@@ -0,0 +1,10 @@
+%% -*- erlang-indent-level: 2 -*-
+%% @author Daniel Luna <daniel@lunas.se>
+%% @copyright 2011 Daniel Luna
+
+-module(fix).
+-author('Daniel Luna <daniel@lunas.se>').
+-export([decode/1]).
+
+decode(String) ->
+  throw(not_yet_implemented).
